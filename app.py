697aed1083a1b0e0c3cbb9a8637cb7142769e15f
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-05-17 18:44:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,40 @@
 import torch
 import soundfile as sf
 from kokoro import KPipeline
-from pydantic import BaseModel, Field
-from typing import Optional, List
-import inferless
+# from pydantic import BaseModel, Field
+# from typing import Optional, List
+# import inferless
 import base64
 import io
 
-@inferless.request
-class RequestObjects(BaseModel):
-    text: str = Field(default="Hello world, this is a test.")
-    voice: Optional[str] = "af_heart"
-    speed: Optional[float] = 1.0
-    split_pattern: Optional[str] = r'\n+'
+# @inferless.request
+# class RequestObjects(BaseModel):
+#     text: str = Field(default="Hello world, this is a test.")
+#     voice: Optional[str] = "af_heart"
+#     speed: Optional[float] = 1.0
+#     split_pattern: Optional[str] = r'\n+'
 
-@inferless.response
-class ResponseObjects(BaseModel):
-    audio_base64: List[str] = Field(default="Test output")
-    graphemes: List[str] = Field(default="Test output")
-    phonemes: List[str] = Field(default="Test output")
+# @inferless.response
+# class ResponseObjects(BaseModel):
+#     audio_base64: List[str] = Field(default="Test output")
+#     graphemes: List[str] = Field(default="Test output")
+#     phonemes: List[str] = Field(default="Test output")
 
 class InferlessPythonModel:
     def initialize(self):
         self.pipeline = KPipeline(lang_code='a')
 
-    def infer(self, request: RequestObjects) -> ResponseObjects:
+    def infer(self, inputs):
+        text = inputs['text']
+        voice = inputs.get("voice","af_heart")
+        speed = inputs.get("speed",1.0)
+        split_pattern = inputs.get("split_pattern",'\n+')
+        
         generator = self.pipeline(
-            request.text,
-            voice=request.voice,
-            speed=request.speed,
-            split_pattern=request.split_pattern
+            text,
+            voice=voice,
+            speed=speed,
+            split_pattern=split_pattern
         )
 
         audio_base64_list = []
@@ -46,11 +51,16 @@ class InferlessPythonModel:
             graphemes.append(gs)
             phonemes.append(ps)
 
-        return ResponseObjects(
-            audio_base64=audio_base64_list,
-            graphemes=graphemes,
-            phonemes=phonemes
-        )
+        return {
+            "audio_base64":audio_base64_list,
+            "graphemes":graphemes,
+            "phonemes":phonemes
+        }
+        # return ResponseObjects(
+        #     audio_base64=audio_base64_list,
+        #     graphemes=graphemes,
+        #     phonemes=phonemes
+        # )
 
     def finalize(self):
         self.pipeline = None
