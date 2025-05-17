875f11accf6e16239a3de379cf386dd19a016f1f
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-05-17 18:40:38 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,26 @@
+INPUT_SCHEMA = {
+    "text": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["Welcome to this beautiful world."]
+    },
+    "voice": {
+        'datatype': 'STRING',
+        'required': False,
+        'shape': [1],
+        'example': ["af_heart"]
+    },
+    "speed": {
+        'datatype': 'FP32',
+        'required': False,
+        'shape': [1],
+        'example': [1.0]
+    },
+    "split_pattern": {
+        'datatype': 'STRING',
+        'required': False,
+        'shape': [1],
+        'example': ["\n+"]
+    }
+}
