019182ec7b3dbd7a43e60bdc88dc0705ccd4da8f
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-05-19 11:50:38 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -4,18 +4,6 @@ INPUT_SCHEMA = {
         'required': True,
         'shape': [1],
         'example': ["The sky above the port was the color of television, tuned to a dead channel."]
-    },
-    "voice": {
-        'datatype': 'STRING',
-        'required': False,
-        'shape': [1],
-        'example': ["af_heart"]
-    },
-    "split_pattern": {
-        'datatype': 'STRING',
-        'required': False,
-        'shape': [1],
-        'example': ["\n+"]
     }
 }
 IS_STREAMING_OUTPUT = True
