e7fad0b3f71c3d689a7f01ddb2c349b182f0a4e6
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-05-19 11:20:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,33 +9,38 @@ class InferlessPythonModel:
         self.pipeline = KPipeline(lang_code='a')
 
     def infer(self, inputs, stream_output_handler):
-        text = inputs['text']
-        voice = inputs.get("voice","af_heart")
-        # speed = inputs.get("speed",1.0)
-        split_pattern = inputs.get("split_pattern",'\n+')
-        
-        generator = self.pipeline(
-            text,
-            voice=voice,
-            speed=1.0,
-            split_pattern=split_pattern
-        )
-        audio_base64_list = []
-        graphemes = []
-        phonemes = []
-
-        for gs, ps, audio in generator:
-            sample_rate = 24000
-            buffer = io.BytesIO()
-            sf.write(buffer, audio, sample_rate, format='WAV')
-            buffer.seek(0)
-            base64_audio = base64.b64encode(buffer.read()).decode('utf-8')
-            print(base64_audio[:100],flush=True)
-            output_dict = {}
-            output_dict["OUT"] = base64_audio
-            stream_output_handler.send_streamed_output(output_dict)
+        try:
+            text = inputs['text']
+            voice = inputs.get("voice","af_heart")
+            # speed = inputs.get("speed",1.0)
+            split_pattern = inputs.get("split_pattern",'\n+')
             
-        stream_output_handler.finalise_streamed_output()
+            generator = self.pipeline(
+                text,
+                voice=voice,
+                speed=1.0,
+                split_pattern=split_pattern
+            )
+            audio_base64_list = []
+            graphemes = []
+            phonemes = []
+    
+            for gs, ps, audio in generator:
+                sample_rate = 24000
+                buffer = io.BytesIO()
+                sf.write(buffer, audio, sample_rate, format='WAV')
+                buffer.seek(0)
+                base64_audio = base64.b64encode(buffer.read()).decode('utf-8')
+                print(base64_audio[:100],flush=True)
+                output_dict = {}
+                output_dict["OUT"] = base64_audio
+                
+                stream_output_handler.send_streamed_output(output_dict)
+                
+            stream_output_handler.finalise_streamed_output()
 
+        except e as Exception:
+            print(e,flush=True)
+            
     def finalize(self):
         self.pipeline = None
