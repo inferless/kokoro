3d68c86fa55f30b586081c1c991206c44616d65a
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-05-19 16:13:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ class InferlessPythonModel:
     def infer(self, inputs, stream_output_handler):
         text = inputs['text']
         voice = inputs.get("voice","af_heart")
-        # speed = inputs.get("speed",1.0)
         split_pattern = inputs.get("split_pattern",'\n+')
         
         generator = self.pipeline(
@@ -20,16 +19,13 @@ class InferlessPythonModel:
             speed=1.0,
             split_pattern=split_pattern
         )
-        audio_base64_list = []
-        graphemes = []
-        phonemes = []
 
         for gs, ps, audio in generator:
             buffer = io.BytesIO()
             sf.write(buffer, audio, samplerate=24000, format='WAV')
             audio_bytes = buffer.getvalue()
             audio_base64 = base64.b64encode(audio_bytes).decode('utf-8')
-            stream_output_handler.send_streamed_output({"generated_audio" : audio_base64})
+            stream_output_handler.send_streamed_output({"OUT" : audio_base64})
             
         stream_output_handler.finalise_streamed_output()
 
